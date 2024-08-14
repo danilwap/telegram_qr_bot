6dfcd3df41bc09c04d288396d3829a2878e72884
Danila Beskrokov <beskrokovd@mail.ru>
2024-08-14 13:23:43 +0300

    add all commands

--- src/app.py
+++ src/app.py
@@ -2,11 +2,10 @@ import logging, asyncio
 import sys
 
 from aiogram import F, types
-from aiogram.filters.callback_data import CallbackData
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, InputFile
-from aiogram.filters import CommandStart, IS_MEMBER, Command
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import Message, FSInputFile
+from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
 from aiogram.filters.state import StatesGroup, State
 from bot import dp, bot
 from utils.create_qr.create_QR import create_qr
@@ -26,9 +25,9 @@ list_status_subscribe = ['creator', 'administrator', 'member', 'restricted']
 async def not_subscribe_channel(message: Message) -> None:
 
     builder_not_subscribe = InlineKeyboardBuilder()
-    builder_not_subscribe.button(text='Подписаться на канал', url='https://t.me/danila_beskrokov')
-    builder_not_subscribe.button(text='Я подписался', url='https://t.me/danila_beskrokov')
-    builder_not_subscribe.button(text='Вернуться в меню', callback_data='/menu')
+    builder_not_subscribe.row(InlineKeyboardButton(text='Подписаться на канал', url='https://t.me/danila_beskrokov'))
+    builder_not_subscribe.row(InlineKeyboardButton(text='Я подписался', callback_data='/menu'))
+
     await message.answer_photo(photo='AgACAgIAAxkBAAO9ZrkiXhTVuvCJvkt3mqB7aB4Z4rkAAjbhMRtsDslJq7x3TmCTeh4BAAMCAAN4AAM1BA', caption="Это бот, в котором вы можете сгенерировать qr-код для вашей ссылки или текста. "
                          "Для того, чтобы пользоваться ботом вам нужно всего лишь подписаться на канал по кнопке "
                          "в сообщении и нажать я подписался", reply_markup=builder_not_subscribe.as_markup())
@@ -36,14 +35,17 @@ async def not_subscribe_channel(message: Message) -> None:
 
 @dp.message(Command(commands=["start"]))
 async def command_start_handler(message: Message, state: FSMContext) -> None:
-    await state.clear()
+    if state:
+        await state.clear()
     user_channel_status = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=message.from_user.id)
 
     if user_channel_status.status in list_status_subscribe:
         builder_is_subscribe = InlineKeyboardBuilder()
-        builder_is_subscribe.button(text='Создать qr-код', callback_data='create_qr')
-        builder_is_subscribe.button(text='Другие полезные боты', callback_data='Other_bots')
-        builder_is_subscribe.button(text='Поблагодарить автора бота', callback_data='gift_creator')
+
+        builder_is_subscribe.row(InlineKeyboardButton(text='Создать qr-код', callback_data='create_qr'))
+        builder_is_subscribe.row(InlineKeyboardButton(text='Другие полезные боты', callback_data='other_bots'))
+        builder_is_subscribe.row(InlineKeyboardButton(text='Поблагодарить автора бота', callback_data='gift_creator'))
+        builder_is_subscribe.row(InlineKeyboardButton(text='Заказать разработку бота', callback_data='order_bot'))
         await message.answer_photo(photo='AgACAgIAAxkBAAO9ZrkiXhTVuvCJvkt3mqB7aB4Z4rkAAjbhMRtsDslJq7x3TmCTeh4BAAMCAAN4AAM1BA', caption="Добрый день! В данном боте вы можете создать и скачать qr-код для ссылки или какого-либо текста"
                              " или перейти в другие полезные боты", reply_markup=builder_is_subscribe.as_markup())
 
@@ -56,14 +58,16 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
 
 @dp.callback_query(F.func(lambda c: c.data and c.data == 'create_qr'))
 async def command_start_handler2(callback_query: types.CallbackQuery, state: FSMContext) -> None:
-    await state.clear()
+    if state:
+        await state.clear()
     user_channel_status = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=callback_query.from_user.id)
 
     if user_channel_status.status in list_status_subscribe:
-        builder_is_subscribe = InlineKeyboardBuilder()
-        builder_is_subscribe.button(text='Вернуться в меню', callback_data='/menu')
-        #builder_is_subscribe.button(text='Другие полезные боты', callback_data='other_bots')
-        await callback_query.message.answer("Пришлите ссылку или текст, для которой надо создать qr-код")
+        keyboards_create_qr = InlineKeyboardBuilder()
+        keyboards_create_qr.button(text='Вернуться в меню', callback_data='/menu')
+        keyboards_create_qr.button(text='Другие полезные боты', callback_data='other_bots')
+        await callback_query.message.answer("Пришлите ссылку или текст, для которой надо создать qr-код",
+                                            reply_markup=keyboards_create_qr.as_markup())
         await state.set_state(QR_State.enter_text)
     else:
         await not_subscribe_channel(callback_query.message)
@@ -86,19 +90,62 @@ async def get_size_qr(message: Message, state: FSMContext):
         user_data = await state.get_data()
         await message.answer(f'Запрос принят! 💭Уже создаю ответ!')
         name_file = create_qr(user_data['enter_text'], user_data['choosing_size_qr'], message.from_user.id)
-        photo = InputFile(f"{name_file}")
-        await message.answer_photo(photo=photo)
+        photo = FSInputFile(f"{name_file}")
+
+        keyboard_qr = InlineKeyboardBuilder()
+
+        keyboard_qr.row(InlineKeyboardButton(text='Создать ещё', callback_data='create_qr'))
+        keyboard_qr.row(InlineKeyboardButton(text='Отблагодарить автора бота', callback_data='gift_creator'))
+        keyboard_qr.row(InlineKeyboardButton(text='Вернуться в меню', callback_data='/menu'))
+
+
+        await message.answer_photo(photo=photo, reply_markup=keyboard_qr.as_markup())
 
         await state.clear()
     else:
         await message.answer('Напишите размер qr-кода от 1 до 10')
 
 
+# Команда другие боты
+@dp.callback_query(F.func(lambda c: c.data == 'other_bots'))
+async def other_bots(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    await state.clear()
+
+
+    other_bots_buttons = InlineKeyboardBuilder()
+    other_bots_buttons.row(InlineKeyboardButton(text='Вернуться в меню', callback_data='/menu'))
+    await callback_query.message.answer('В будущем тут будут полезные боты, созданные мной!',
+                                        reply_markup=other_bots_buttons.as_markup())
+
+
+
+# Команда поблагодарить автора
+@dp.callback_query(F.func(lambda c: c.data == 'gift_creator'))
+async def gift_creator(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    await state.clear()
+    gift_creator_buttons = InlineKeyboardBuilder()
+    gift_creator_buttons.row(InlineKeyboardButton(text='Вернуться в меню', callback_data='/menu'))
+    await callback_query.message.answer('Буду очень признателен за любую благодарность. '
+                                        'Можно перевести по номеру карты `2200701026272721` '
+                                        '(Просто нажмите на номер, чтобы скопировать)', parse_mode= 'markdown',
+                                        reply_markup=gift_creator_buttons.as_markup())
+
+
+# Команда заказать бота
+@dp.callback_query(F.func(lambda c: c.data == 'order_bot'))
+async def order_bot(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    await state.clear()
+    order_bot_buttons = InlineKeyboardBuilder()
+    order_bot_buttons.row(InlineKeyboardButton(text='Заказать бот', url='https://t.me/danila_in'))
+    order_bot_buttons.row(InlineKeyboardButton(text='Вернуться в меню', callback_data='/menu'))
+    await callback_query.message.answer(text='Для того, чтобы заказать бота, напишите мне в личку: "Хочу бота!" и '
+                                             'я вам помогу в этом вопросе', reply_markup=order_bot_buttons.as_markup())
+
+
 # Команда меню
 @dp.callback_query(F.func(lambda c: c.data and c.data.startswith('/menu')))
-async def request_menu(callback_query: types.CallbackQuery) -> None:
-    print(123)
-    await command_start_handler(callback_query.message)
+async def request_menu(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    await command_start_handler(callback_query.message, state)
 
 
 #@dp.message()
@@ -108,10 +155,16 @@ async def get_id_file(message: Message):
 
 @dp.message()
 async def all_unexpected_messages(message: Message, state: FSMContext):
-    await message.answer_photo(photo='AgACAgIAAxkBAAO3ZrkhabyWDSfo3tA7S-dKkm-uSS8AAjHhMRtsDslJceOHEKevqVsBAAMCAAN4AAM1BA', caption='Я пока не знаю как ответить на это сообщение, я ещё только учусь, но любовь помогает нам делать настоящие чудеса.')
+    user_channel_status = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=message.from_user.id)
+
+    if user_channel_status.status in list_status_subscribe:
+        await message.answer_photo(photo='AgACAgIAAxkBAAO3ZrkhabyWDSfo3tA7S-dKkm-uSS8AAjHhMRtsDslJceOHEKevqVsBAAMCAAN4AAM1BA',
+                               caption='Я пока не знаю как ответить на это сообщение, я ещё только учусь, '
+                                       'но в будущем я смогу ответить на этот вопрос.')
 
 
-
+    else:
+        await not_subscribe_channel(message)
 
 async def main() -> None:
     await dp.start_polling(bot)
--- src/utils/create_qr/create_QR.py
+++ src/utils/create_qr/create_QR.py
@@ -1,11 +1,12 @@
 import qrcode
-
+import datetime
 
 
 def create_qr(text: str, size: str, id: int):
-
+    start_time = datetime.datetime.now()
     qr = qrcode.make(text, border=1, box_size=int(size)*10)
     qr.save(f'result{id}.png')
+    print('Затраченное время:', datetime.datetime.now() - start_time)
     return f'result{id}.png'
 
 
